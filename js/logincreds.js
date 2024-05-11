dcfde4cd9c977e775fb6cfda284da972ab81b10a
Leandro R. Morales <lea.moralesm@duocuc.cl>
2024-05-10 21:08:44 -0400

    Añadido nuevos fondos y pagina registro admin en proceso

--- js/logincreds.js
+++ js/logincreds.js
@@ -1,20 +1,42 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const registrationForm = document.getElementById("registrationForm");
-  
+  const registrationForm = document.getElementById("registrationForm");
+
+  if (registrationForm) { // Verificar si el formulario de registro está presente en la página
     registrationForm.addEventListener("submit", function(event) {
-      event.preventDefault(); // Prevent default form submission
-  
-      // Get the values from the registration form
+      event.preventDefault(); // Evitar el envío predeterminado del formulario
+
+      // Obtener los valores del formulario
       const username = document.getElementById("username").value;
       const email = document.getElementById("email").value;
       const password = document.getElementById("password").value;
-  
-      // Perform client-side validation (if needed)
-  
-      // Save email and password in localStorage (you can adjust this as per your needs)
+      let adminKey = ''; // Inicializar la clave de administrador como vacía
+
+      // Verificar si se proporcionó una adminKey
+      const adminKeyInput = document.getElementById("adminKey");
+      if (adminKeyInput) {
+        adminKey = adminKeyInput.value;
+      }
+
+      // Guardar email, contraseña y clave de administrador (si existe) en localStorage
       localStorage.setItem("username", username);
       localStorage.setItem("email", email);
       localStorage.setItem("password", password);
+      if (adminKey) {
+        localStorage.setItem("adminKey", adminKey);
+      }
+
+      // Redirigir al usuario de vuelta al inicio
+      window.location.href = "Inicio.html";
     });
-  });
-  
+  }
+});
+
+function generateRandomKey() {
+  const allowedChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789#_';
+  let key = '';
+  for (let i = 0; i < 16; i++) {
+    const randomIndex = Math.floor(Math.random() * allowedChars.length);
+    key += allowedChars.charAt(randomIndex);
+  }
+  return key;
+}
