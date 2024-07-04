8d7494667cb0d7acebf9e54a486aed9395675d3e
SylvaThief <132790939+SylvaThief@users.noreply.github.com>
2024-07-04 16:29:47 -0400

    Update models.py

--- vehiculos_project/autos/models.py
+++ vehiculos_project/autos/models.py
@@ -1,4 +1,5 @@
 # models.py
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Vehiculo(models.Model):
@@ -13,3 +14,10 @@ class Vehiculo(models.Model):
 class Venta(models.Model):
     vehiculo = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)
     fecha_venta = models.DateTimeField(auto_now_add=True)
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    admin_key = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
