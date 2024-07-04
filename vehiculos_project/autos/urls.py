f1021587f4dbcfbe4dc706bfcb9c1d45c09493b6
SylvaThief <132790939+SylvaThief@users.noreply.github.com>
2024-07-04 16:58:52 -0400

    Update urls.py

--- vehiculos_project/autos/urls.py
+++ vehiculos_project/autos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import VehiculoViewSet, VentaViewSet, get_vehiculos_mas_vendidos
 from .views import login_view
+from .views import register_view
 
 router = DefaultRouter()
 router.register(r'vehiculos', VehiculoViewSet)
@@ -11,4 +12,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('vehiculos_mas_vendidos/', get_vehiculos_mas_vendidos, name='vehiculos_mas_vendidos'),
     path('login/', login_view, name='login'),
+    path('register/', register_view, name='register'),
 ]
