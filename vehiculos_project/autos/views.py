c4b0f4d9ef84b1f49c45103e9261c22759b20bf6
SylvaThief <132790939+SylvaThief@users.noreply.github.com>
2024-07-04 16:42:06 -0400

    Update views.py

--- vehiculos_project/autos/views.py
+++ vehiculos_project/autos/views.py
@@ -5,6 +5,32 @@ from rest_framework.response import Response
 from django.db.models import Count
 from .models import Vehiculo, Venta
 from .serializers import VehiculoSerializer, VentaSerializer
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import LoginForm
+
+def login_view(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            admin_key = form.cleaned_data['admin_key']
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('home')
+            else:
+                messages.error(request, 'Nombre de usuario o contraseña incorrectos.')
+    else:
+        form = LoginForm()
+
+    return render(request, 'Inicio.html', {'form': form})
+
 
 class VehiculoViewSet(viewsets.ModelViewSet):
     queryset = Vehiculo.objects.all()
