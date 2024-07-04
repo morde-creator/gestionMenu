23e018fbb047ea03e814167e519a326c96f4cc2c
SylvaThief <132790939+SylvaThief@users.noreply.github.com>
2024-07-04 16:53:18 -0400

    Update views.py

--- vehiculos_project/autos/views.py
+++ vehiculos_project/autos/views.py
@@ -29,7 +29,7 @@ def login_view(request):
     else:
         form = LoginForm()
 
-    return render(request, 'Inicio.html', {'form': form})
+    return render(request, 'login/login.html', {'form': form})
 
 
 class VehiculoViewSet(viewsets.ModelViewSet):
